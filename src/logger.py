1fbe7134345fd87e83fea17fa5863d96ab6b1920
maddibalaj <maddi.24mim10049@vitbhopal.ac.in>
2026-06-19 18:37:37 +0530

    flask completed

--- src/logger.py
+++ src/logger.py
@@ -6,6 +6,8 @@ logs_paths = os.path.join(os.getcwd(),"logs",LOG_FILE)
 os.makedirs(logs_paths, exist_ok=True)
 LOG_FILE_PATH = os.path.join(logs_paths, LOG_FILE)
 
-logging.basicConfig(filename=LOG_FILE_PATH, level=logging.INFO,format='[%(asctime)s] %(name)s - %(levelname)s - %(message)s')
-if __name__=="__main__":
-    logging.info("Logging has started") 
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,  
+)
